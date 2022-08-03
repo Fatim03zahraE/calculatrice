9e1bc0da25a0cd8c695a16c22725bc77dcfabf32
Fatim03zahraE <fatizahrae2001zouiti@gmail.com>
2022-08-03 17:34:42 +0200

    code1

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int main()
+{
+    float A,B;
+    char op,ch;
+   do{
+        printf("\nEntrer le premier nombre\n");
+     scanf("%f",&A);
+     printf("Entrer le deuxieme  nombre\n");
+     scanf("%f",&B);
+     printf("Entrer un operateur\n");
+     scanf("%s",&op);
+     switch(op)
+     {
+            case '+' :
+        printf("%f+%f =%f",A,B,A+B);
+        break;
+    case '-':
+        printf("%f-%f =%f",A,B,A-B);
+        break;
+    case '*':
+        printf("%f*%f =%f",A,B,A*B);
+        break;
+    case '/':
+        if(A!=0)
+        {
+            printf("%f/%f =%f",A,B,A/B);
+        }
+        else{
+           printf("erreur");
+        }
+        break;
+    default:
+        printf("erreur");
+     }
+} while(op!='q');
+    return 0;
+}
